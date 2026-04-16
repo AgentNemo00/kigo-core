1a04ff5c2adc1432d63e51a040c4ac70049a3c23
Francisco Cardoso <francisco.cardoso.0@icloud.com>
2026-04-16 13:01:32 +0200

    fix const for notifications

--- notification/const.go
+++ notification/const.go
@@ -4,4 +4,6 @@ const (
     // Basic notifications from the modules to the main service
 	NotificationReady 			= "NotificationReady" // called when the module is ready to receive orders, i.e. after startup procedore, Publish to main
 	NotificationUpdate 			= "NotificationUpdate" // called when the module has an update to share, i.e. update data or state, Publish to main
+	NotificationInformation 	= "NotificationInformation" // called when the module has information to share, i.e. about itself or the environment, Publish to main
+	NotificationRender 			= "NotificationRender" // called when the module has a render to share, i.e. a string to render, Publish to main
 )
