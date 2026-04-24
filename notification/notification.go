65f9ba63447759979b17371ff02ee9f6d4ef5897
Francisco Cardoso <francisco.cardoso.0@icloud.com>
2026-04-24 19:26:50 +0200

    removed double field definition

--- notification/notification.go
+++ notification/notification.go
@@ -7,7 +7,6 @@ import (
 
 type Notification struct {
 	From 			string 	// ID from who the message is. Module ID
-	FromName 		string	// Human readable name of the module, for debugging and logging purposes
 	To 				string	// ID to who the message is. KiGo ID
 	Notification	string  // what notification to publish, e.g. ready, update, render
 	Payload 		any
@@ -15,7 +14,7 @@ type Notification struct {
 
 type NotificationReadyPayload struct {
 	Duration 		time.Duration // Duration needed to be ready, when should order startup be called
-	Name 			string // Human readable name of the module, for debugging and logging purposes
+	Name 			string // Human readable name of the module, for debugging and logging purposes  
 	Changes         []string // List of changes that the module has, e.g. update, render, etc. For debugging and logging purposes
 	Heartbeat 		time.Duration // Duration between each heartbeat, for debugging and logging purposes
 }
